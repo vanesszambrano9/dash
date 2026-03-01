4143514329d8aef2adaeab2ba737040bc49effa1
Vanessa Lainez <vanessa.lainez@ingeniacode.com>
2026-02-28 20:01:33 -0600

    .

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,37 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Create permissions
+        $permissions = [
+            'acceder-dashboard',
+            'acceder-userlist_Iac',
+            'acceder-administrar-roles',
+            'acceder-permissions-list',
+            'acceder-sessionlist_p6D',
+        ];
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate(['name' => $permission]);
+        }
+
+        // Create admin role
+        $adminRole = Role::firstOrCreate(['name' => 'admin_general']);
+        
+        // Assign all permissions to admin role
+        $adminRole->syncPermissions(Permission::all());
+
+        // Create admin user
+        $user = User::firstOrCreate(
+            ['email' => 'admin@dash.com'],
+            [
+                'name' => 'Admin User',
+                'password' => bcrypt('12345678'),
+            ]
+        );
+
+        // Assign admin role to user
+        if (!$user->hasRole('admin_general')) {
+            $user->assignRole('admin_general');
+        }
     }
 }
